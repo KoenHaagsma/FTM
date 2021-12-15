b776d8978797456488da5cf2d0102e4f4de5a343
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-12-15 09:43:01 +0100

    Clean branch

--- index.js
+++
@@ -1,8 +0,0 @@
-const stream = require('fs').createReadStream('./input/nabijheid_excl_shapes.csv');
-const reader = require('readline').createInterface({ input: stream });
-const arr = [];
-
-reader.on('line', (row) => {
-    arr.push(row.split(';'));
-    console.log(arr);
-});
