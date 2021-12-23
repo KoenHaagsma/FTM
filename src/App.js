32de72da38b7712f0ad551fb018ee8d4aef18556
Jabir Tisoudali <44145503+jabirtisou@users.noreply.github.com>
2021-12-23 16:25:04 +0100

    Postcode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import * as d3 from 'd3';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { csv } from "d3";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [postcode, setPostcode] = useState("");
+  const [output, setOutput] = useState(null);
+  const [dataShapes, setDataShapes] = useState(null);
 
   useEffect(() => {
-    d3.csv('nabijheid_excl_shapes.csv', d3.autoType).then((data) => {
-      setData(data);
-    });
-  }, []);
-  console.log(data);
+    const getData = async () => {
+      await fetch("/data_shapes.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setDataShapes(data);
+        });
 
-  return <div className='App'>hallo</div>;
+      const response = await csv("./output.csv");
+      setOutput(response);
+    };
+
+    getData();
+  }, []);
+
+  const handleClick = () => {
+    const areaCode = postcode.toUpperCase();
+    let result = output.filter((d) => d.PC6 === areaCode)[0];
+    let dataShape = dataShapes.filter((d) =>
+      d.GWB_CODE.includes(result.Buurt2019)
+    );
+    console.log(dataShape);
+  };
+
+  return (
+    <div className="container">
+      <div className="area-code">
+        <input
+          placeholder="Postcode"
+          maxLength="6"
+          id="code"
+          value={postcode}
+          onChange={(e) => setPostcode(e.target.value)}
+        />
+        <button onClick={handleClick}>Zoek andere wijk</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
